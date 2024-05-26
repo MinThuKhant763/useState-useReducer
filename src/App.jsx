5d22a0dbede4ddd873c7bc26ea68a9dde38d2a03
MinThuKhant763 <85979978+MinThuKhant763@users.noreply.github.com>
2024-05-26 23:21:31 +0630

    useReducer Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useReducer } from 'react';
+import './App.css';
 
+const firstReducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count++ };
+    case "decrement":
+      return { ...state, count: state.count-- };
+    case "updateKey":
+      return { ...state, key: action.payload };
+    default:
+      throw new Error();
+  }
+};
+
+const ACTION = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+  UPDATE_KEY: "updateKey"
+}
 function App() {
-  const [key, setKey] = useState("");
-  const [count,setCount] = useState(0)
- 
+  const [state, dispatch] = useReducer(firstReducer, { count: 0, key: "" });
+
+  // const [key, setKey] = useState("");
+  // const [count,setCount] = useState(0);
+
   return (
     <div>
-      <input type="text" onChange={ (e) => setKey(e.target.value) } />
-      <h3>your key is - { key }</h3>
-      <button onClick={()=> setCount((prev)=>prev - 1)}> - </button>
-      <span>{count}</span>
-      <button onClick={ () => setCount((prev) => prev + 1)}> + </button>
+      <input type="text" onChange={ (e) => dispatch({ type: ACTION.UPDATE_KEY, payload: e.target.value }) } />
+      <h3>your key is - { state.key }</h3>
+      <button onClick={ () => dispatch({ type: ACTION.DECREMENT}) }> - </button>
+      <span>{ state.count }</span>
+      <button onClick={ () => dispatch({ type: ACTION.INCREMENT }) }> + </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
